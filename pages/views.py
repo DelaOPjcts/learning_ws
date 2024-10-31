2b1a2d9bcb7e1d17a92806f1f85be005a77ee8aa
Daniel De la O <delao@MacBook-Air-de-Daniel.local>
2024-10-31 12:11:10 -0600

    home/about

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_page_view(request):
+    return HttpResponse("Homepage")
+
+def about_page_view(request):
+    #return render(request, "pages/about.html")
+    context = {"name": "Daniel",
+               "age": 37}
+    return render(request, "pages/about.html", context)
